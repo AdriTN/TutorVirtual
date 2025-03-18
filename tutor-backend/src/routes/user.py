2606491365067bb60a184e0ac66bd42405bc75a5
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-18 11:23:03 +0000

    Endpoints CRUD para usuarios

--- tutor-backend/src/routes/user.py
+++ tutor-backend/src/routes/user.py
@@ -1,20 +1,20 @@
 from fastapi import APIRouter, HTTPException
-from src.database import get_connection
+from ..database.database import get_connection
 
 router = APIRouter()
 
 @router.post("/users/", status_code=201)
-def create_user(name: str, email: str):
+def create_user(name: str, email: str, password: str):
     conn = get_connection()
     if not conn:
         raise HTTPException(status_code=500, detail="No hay conexión a la BD")
     try:
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s) RETURNING id", (name, email))
+        cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s) RETURNING id", (name, email, password))
         user_id = cursor.fetchone()[0]
         conn.commit()
         cursor.close()
-        return {"id": user_id, "name": name, "email": email}
+        return {"id": user_id, "name": name, "email": email, "password": password}
     except Exception as e:
         conn.rollback()
         raise HTTPException(status_code=400, detail=str(e))
@@ -27,12 +27,12 @@ def get_users():
     if not conn:
         raise HTTPException(status_code=500, detail="No hay conexión a la BD")
     cursor = conn.cursor()
-    cursor.execute("SELECT id, name, email FROM users")
+    cursor.execute("SELECT id, name, email, password FROM users")
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
 
     users = []
     for row in rows:
-        users.append({"id": row[0], "name": row[1], "email": row[2]})
+        users.append({"id": row[0], "name": row[1], "email": row[2], "password": row[3]})
     return users
