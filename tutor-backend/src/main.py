356cf6a3afe7fe277f196d22fb651a1a6a0ef8c2
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-18 11:10:47 +0000

    Añadimos un lifespan para crear la tabla de usuarios

--- tutor-backend/src/main.py
+++ tutor-backend/src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from src.models.user import create_users_table
+
 app = FastAPI()
 
 app.title = "Tutor Virtual"
@@ -8,3 +10,7 @@ app.version = "0.0.1"
 @app.get("/", tags=["home"])
 def home():
     return {"Hello": "World"}
+
+@app.lifespan("startup")
+def on_startup():
+    create_users_table()
