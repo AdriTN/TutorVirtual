047f9fe77364e9b6d7aba61087de7419d89b3bec
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-18 11:11:11 +0000

    Mejoramos la conexión con la base de datos

--- tutor-backend/src/database/database.py
+++ tutor-backend/src/database/database.py
@@ -5,8 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    connection = psycopg2.connect(
+    try:
+        connection = psycopg2.connect(
         dsn=os.getenv("DATABASE_URL"),
         client_encoding='utf8')
-    
-    return connection
+        
+        return connection
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
