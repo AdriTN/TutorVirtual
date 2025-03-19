e6155c6952d34c0634e26d9b6b5e07af79bff736
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-19 19:37:28 +0000

    Creamos endpoints para obtener información personal (solo usuario) e información de los usuarios (solo admin)

--- tutor-backend/src/models/user.py
+++ tutor-backend/src/models/user.py
@@ -10,6 +10,7 @@ def create_users_table():
             username VARCHAR(120) NOT NULL,
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(100) NOT NULL,
+            is_admin BOOLEAN NOT NULL DEFAULT false,
             
             CONSTRAINT chk_name_min_length CHECK (LENGTH(username) >= 3),
             CONSTRAINT chk_password_complexity CHECK (password ~* '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$')
