7ffc534c5dfd849d2280b2c8b7bb1c6ebb125a94
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-20 18:42:41 +0000

    Actualizamos la nueva ruta de importación del home

--- tutor-frontend/src/App.tsx
+++ tutor-frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
