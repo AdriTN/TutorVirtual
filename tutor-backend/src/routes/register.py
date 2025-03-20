5586bbefa63011372433c496e9f3c9a4c096d558
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-20 17:22:38 +0000

    Añadimos una clase que represente el body

--- tutor-backend/src/routes/register.py
+++ tutor-backend/src/routes/register.py
@@ -1,22 +1,28 @@
 from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel
 from ..database.database import get_connection
 from ..auth.auth import hash_password, verify_password, create_jwt_token
 
 router = APIRouter()
 
+class RegisterUser(BaseModel):
+    username: str
+    email: str
+    password: str
+
 @router.post("/register/", status_code=201)
-def register(username: str, email: str, password: str):
-    hashed_password = hash_password(password)
+def register(body: RegisterUser):
+    hashed_password = hash_password(body.password)
     conn = get_connection()
     if not conn:
         raise HTTPException(status_code=500, detail="No hay conexión a la BD")
     
     try:
         with conn.cursor() as cursor:
-            cursor.execute("INSERT INTO users (username, email, password, is_admin) VALUES (%s, %s, %s, %s) RETURNING id", (username, email, hashed_password, False))
+            cursor.execute("INSERT INTO users (username, email, password, is_admin) VALUES (%s, %s, %s, %s) RETURNING id", (body.username, body.email, hashed_password, False))
             user_id = cursor.fetchone()[0]
             conn.commit()
-        return {"id": user_id, "name": username, "email": email}
+        return {"id": user_id, "name": body.username, "email": body.email}
     except Exception as e:
         conn.rollback()
         raise HTTPException(status_code=400, detail=str(e))
