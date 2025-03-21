a141f7da4ac7787ad08765824c38f5e55ab66b8b
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-21 11:00:59 +0000

    Añadimos el campo de verificación de contraseña

--- tutor-frontend/src/services/endpoints/authservice.ts
+++ tutor-frontend/src/services/endpoints/authservice.ts
@@ -1,9 +1,10 @@
 import { api } from "../apis/api";
 
-export async function registerUser(username: string, email: string, password: string) {
+export async function registerUser(username: string, email: string, password: string, confirmPassword: string) {
     return await api.post("/api/register", {
         username,
         email,
         password,
+        confirmPassword,
     });
 }
