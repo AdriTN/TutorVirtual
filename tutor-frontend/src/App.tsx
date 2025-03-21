af516c4b412126195bdbe9ed25f842c2504e2193
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-21 08:41:23 +0000

    Añadimos la página de registro al programa principal

--- tutor-frontend/src/App.tsx
+++ tutor-frontend/src/App.tsx
@@ -1,18 +1,11 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import RegisterPage from "./pages/RegisterPage";
+import RegisterPage from "./pages/register/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Pequeña barra de navegación de ejemplo */}
-      <nav style={{ padding: "1rem" }}>
-        <Link to="/" style={{ marginRight: "1rem" }}>Inicio</Link>
-        <Link to="/register">Registro</Link>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
