0f2f9e6cb53db41f0a86db534291b79899dff5a6
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-21 10:59:53 +0000

    Cambiamos la ruta de los endpoints por simplicidad

--- tutor-backend/src/routes/user.py
+++ tutor-backend/src/routes/user.py
@@ -6,7 +6,7 @@ from ..auth.auth_dependencies import admin_required, jwt_required
 router = APIRouter()
 
 # Endpoints para usuarios
-@router.post("/users/", status_code=201)
+@router.post("/users", status_code=201)
 def create_user(name: str, email: str, password: str):
     conn = get_connection()
     if not conn:
@@ -31,7 +31,7 @@ def create_user(name: str, email: str, password: str):
     finally:
         conn.close()
 
-@router.get("/users/me/")
+@router.get("/users/me")
 def get_my_data(playload: dict = Depends(jwt_required)):
     user_id = playload.get("user_id")
     conn = get_connection()
@@ -48,7 +48,7 @@ def get_my_data(playload: dict = Depends(jwt_required)):
     
     return {"id": user[0], "name": user[1], "email": user[2]}
 
-@router.delete("/users/{id}/")
+@router.delete("/users/{id}")
 def delete_user(id: int):
     conn = get_connection()
     if not conn:
@@ -85,7 +85,7 @@ def promote_to_admin(user_id: int, playload: dict = Depends(admin_required)):
     finally:
         conn.close()
 
-@router.get("/users/")
+@router.get("/users")
 def get_users(playload: dict = Depends(admin_required)):
     conn = get_connection()
     if not conn:
