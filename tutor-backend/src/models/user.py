f866c40dfa8950d8ce4edb1832af669c49972509
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-22 12:57:30 +0000

    Añadimos una tabla para los usuarios registrados con proveedores

--- tutor-backend/src/models/user.py
+++ tutor-backend/src/models/user.py
@@ -4,6 +4,7 @@ def create_users_table():
     connection = get_connection()
     cursor = connection.cursor()
     
+    # Crear tabla de usuarios locales
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
@@ -14,6 +15,17 @@ def create_users_table():
             
             CONSTRAINT chk_name_min_length CHECK (LENGTH(username) >= 3),
             CONSTRAINT chk_password_complexity CHECK (password ~* '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$')
+        );
+    """)
+    
+    # Crear tabla de usuarios para almacenar info de cada proveedor
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users_providers (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER NOT NULL REFENCES users(id) ON DELETE CASCADE,
+            provider VARCHAR(50) NOT NULL,
+            provider_user_id VARCHAR(255) NOT NULL,
+            UNIQUE(provider_user_id, provider)
         );
     """)
     
