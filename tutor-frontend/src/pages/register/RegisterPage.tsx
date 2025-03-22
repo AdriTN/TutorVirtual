36c059e9452d9b1626b69f0665f14007f4f095a0
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-22 12:53:42 +0000

    Añadimos el botón a la página de registro de usuario

--- tutor-frontend/src/pages/register/RegisterPage.tsx
+++ tutor-frontend/src/pages/register/RegisterPage.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import RegisterForm from "../../components/register/Register";
 import styles from "./RegisterPage.module.css";
+import MyGoogleButton from "../../components/google/MyGoogleButton";
 
 import.meta.env.VITE_API_URL;
 
 const RegisterPage: React.FC = () => {
     const baseURL = import.meta.env.VITE_BACKEND_URL
     const registerEndpoint = `${baseURL}/api/register`;
+    const googleRegisterEndpoint = `${baseURL}/api/register/google`;
 
   return (
     <div className={styles.pageContainer}>
       <RegisterForm endpointUrl={registerEndpoint} />
+
+      <MyGoogleButton endpointUrl={googleRegisterEndpoint} />
     </div>
   );
 };
