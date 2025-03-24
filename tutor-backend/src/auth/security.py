50f732c915dcd7b53a7ee7431e8ceb371ea141e3
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-24 10:29:54 +0000

    Renombramos el fichero de seguridad para mayor claridad

---
+++ tutor-backend/src/auth/security.py
@@ -0,0 +1,36 @@
+import uuid
+import bcrypt
+import jwt
+import os
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password.decode("utf-8")
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+def create_jwt_token(data: dict, expires_in_minutes=30) -> str:
+    expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=expires_in_minutes)
+    data.update({"exp": expire})
+    token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    
+    return token
+
+def decode_jwt_token(token: str) -> dict:
+    try:
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return decoded
+    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        return None
+
+def create_refresh_token() -> str:
+    return str(uuid.uuid4())
