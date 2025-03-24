120657163173186477f82aa70b11ecf8d5b7ca67
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-24 10:33:37 +0000

    Añadimos la referencia al login

--- tutor-frontend/src/pages/register/RegisterPage.tsx
+++ tutor-frontend/src/pages/register/RegisterPage.tsx
@@ -8,7 +8,7 @@ import.meta.env.VITE_API_URL;
 const RegisterPage: React.FC = () => {
     const baseURL = import.meta.env.VITE_BACKEND_URL
     const registerEndpoint = `${baseURL}/api/register`;
-    const googleRegisterEndpoint = `${baseURL}/api/register/google`;
+    const googleRegisterEndpoint = `${baseURL}/api/google`;
 
   return (
     <div className={styles.pageContainer}>
