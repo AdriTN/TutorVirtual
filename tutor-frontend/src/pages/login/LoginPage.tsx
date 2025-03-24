e5061ff5d35a3038223473db8ba9e2646d46a06f
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-24 10:08:39 +0000

    Página de login con acceso por Google

---
+++ tutor-frontend/src/pages/login/LoginPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import LoginForm from "../../components/login/Login"; // Asegúrate de que la ruta y nombre del componente sean correctos
+import styles from "./LoginPage.module.css";
+import MyGoogleButton from "../../components/google/MyGoogleButton";
+
+const LoginPage: React.FC = () => {
+  const baseURL = import.meta.env.VITE_BACKEND_URL;
+  const loginEndpoint = `${baseURL}/api/login`;
+  const googleLoginEndpoint = `${baseURL}/api/login/google`;
+
+  return (
+    <div className={styles.pageContainer}>
+      <LoginForm endpointUrl={loginEndpoint} />
+      <MyGoogleButton endpointUrl={googleLoginEndpoint} />
+    </div>
+  );
+};
+
+export default LoginPage;
