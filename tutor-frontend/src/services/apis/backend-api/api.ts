b01f0813b7a46692698a7f1a38da2e9ea8e24fe4
Adrián Talavera <adriiantalavera@gmail.com>
2025-03-28 09:59:01 +0000

    Cambiamos la ubicación de la declaración de la api en el frontend

---
+++ tutor-frontend/src/services/apis/backend-api/api.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useAuth } from "../../../context/AuthContext";
+
+
+const apiUrl = import.meta.env.VITE_BACKEND_URL;  // "http://localhost:8000"
+
+export const api = axios.create({
+  baseURL: apiUrl,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      // Aqui necesitamos acceso a "tryRefreshToken" de AuthContext.tsx
+      const { tryRefreshToken } = useAuth();
+
+      const success = await tryRefreshToken();
+
+      if (success) {
+        originalRequest.headers["Authorization"] = `Bearer ${localStorage.getItem("accessToken")}`;
+        return api.request(originalRequest);
+      } else {
+        window.location.href = "/home";
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
+  
