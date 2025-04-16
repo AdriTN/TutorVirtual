45df6379acb081b91ab50554f16478cafa271ea2
Adrián Talavera <adriiantalavera@gmail.com>
2025-04-16 11:53:34 +0100

    Navbar para la pantalla home terminado

--- tutor-frontend/src/pages/utils/NavBar/NavBar.tsx
+++ tutor-frontend/src/pages/utils/NavBar/NavBar.tsx
@@ -1,33 +1,30 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-import { useNavigate } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.nav}>
+      <div className={styles.logo}>
+        <img
+          src="logo.png"
+          alt="Logo"
+          className={styles.logoImage}
+        />
+      
       <a href="/" className={styles.brand}>
         Tutor Virtual
       </a>
+      </div>
       <ul className={styles.navLinks}>
         <li>
-          <a href="/login" className={styles.navbarItem}>
-            Login
+          <a href="/" className={styles.navbarItem}>
+            Cursos
           </a>
         </li>
         <li>
-            <button 
-              className={styles.cta}
-              onClick={() => {
-                const navigate = useNavigate();
-                navigate("/register");
-              }} 
-            >
-              <span>Sign Up</span>
-              <svg width="15px" height="10px" viewBox="0 0 13 10" color="#be45e2">
-                <path d="M1,5 L11,5" />
-                <polyline points="8 1 12 5 8 9" />
-              </svg>
-            </button>
+          <a href="/login" className={styles.navbarItem}>
+            Iniciar Sesión
+          </a>
         </li>
       </ul>
     </nav>
