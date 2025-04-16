514ccf853c5681ef11baafb2e1dec9744a3ebcdf
Adrián Talavera <adriiantalavera@gmail.com>
2025-04-16 16:36:32 +0100

    Añadimos una guestRoute para evitar que el usuario un usuario ya loguado no pueda acceder a las paginas de login o register

--- tutor-frontend/src/context/AuthContext.tsx
+++ tutor-frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect, use } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import api from "../services/apis/backend-api/api";
 
 interface AuthContextProps {
