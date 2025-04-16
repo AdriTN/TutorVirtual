b0a58e9421fd0656ed1ffe94c82dff06f9bddbf4
Adrián Talavera <adriiantalavera@gmail.com>
2025-04-16 12:46:14 +0100

    Pulimos la sección hero del home

--- tutor-frontend/src/pages/utils/HeroSection/HeroSection.tsx
+++ tutor-frontend/src/pages/utils/HeroSection/HeroSection.tsx
@@ -4,29 +4,31 @@ import styles from "./HeroSection.module.css";
 interface HeroSectionProps {
   title: string;
   subtitle: string;
-  buttonLabel: string;
   imageUrl?: string;
   onButtonClick?: () => void;
 }
 
-/**
- * HeroSection es un componente reutilizable 
- * con título, subtítulo, botón y una imagen opcional.
- */
+
 const HeroSection: React.FC<HeroSectionProps> = ({
   title,
   subtitle,
-  buttonLabel,
   imageUrl,
   onButtonClick,
 }) => {
   return (
-    <div className={styles.hero}>
+    <section className={styles.hero}>
       <div className={styles.heroText}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
-        <button className={styles.ctaButton} onClick={onButtonClick}>
-          {buttonLabel}
+        <button className={styles.animatedbutton} onClick={onButtonClick}>
+        <svg viewBox="0 0 24 24" className={styles.arr2}>
+          <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
+        </svg>
+        <span className={styles.text}>Comenzar Ahora</span>
+        <span className={styles.circle} />
+        <svg viewBox="0 0 24 24" className={styles.arr1}>
+          <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
+        </svg>
         </button>
       </div>
       {imageUrl && (
@@ -34,7 +36,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({
           <img src={imageUrl} alt="Hero" />
         </div>
       )}
-    </div>
+    </section>
   );
 };
 
