04c802a3c00a80fb3d631976ef10be3769d47144
Adrián Talavera <adriiantalavera@gmail.com>
2025-05-15 11:32:13 +0100

    Creamos los endpoints para crear y obtener los temas

--- tutor-backend/src/main.py
+++ tutor-backend/src/main.py
@@ -9,6 +9,7 @@ from .routes.register import router as register_router
 from .routes.google import router as google_router
 from .routes.subjects import router as subject_router
 from .routes.course import router as course_router
+from .routes.theme import router as themes_router
 
 from dotenv import load_dotenv
 import os
@@ -48,3 +49,4 @@ app.include_router(register_router, prefix="/api", tags=["Register"])
 app.include_router(google_router, prefix="/api", tags=["Google"])
 app.include_router(subject_router, prefix="/api", tags=["Subjects"])
 app.include_router(course_router, prefix="/api", tags=["Courses"])
+app.include_router(themes_router, prefix="/api", tags=["Themes"])
