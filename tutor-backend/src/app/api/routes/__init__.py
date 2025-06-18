4051ec97c6350dd4c5d92081f0950f498e6b8d2f
Adrián Talavera <adriiantalavera@gmail.com>
2025-06-18 11:31:27 +0100

    Añadimos rutas no contempladas anteriormente

--- tutor-backend/src/app/api/routes/__init__.py
+++ tutor-backend/src/app/api/routes/__init__.py
@@ -9,15 +9,19 @@ from .courses   import router as courses_router
 from .subjects  import router as subjects_router
 from .themes    import router as themes_router
 from .ai        import router as ai_router
-from .answer   import router as answer_router
+from .answer    import router as answer_router
 from .stats     import router as stats_router
+from .logout    import router as logout_router
+from .register  import router as register_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router    , prefix="/auth"   , tags=["Auth"])
+api_router.include_router(logout_router  , prefix="/auth"   , tags=["Auth"])
+api_router.include_router(register_router, prefix="/auth"   , tags=["Auth"])
 api_router.include_router(users_router   , prefix="/users"  , tags=["Users"])
 api_router.include_router(courses_router , prefix="/courses", tags=["Courses"])
 api_router.include_router(subjects_router, prefix="/subjects", tags=["Subjects"])
 api_router.include_router(themes_router  , prefix="/themes" , tags=["Themes"])
 api_router.include_router(ai_router      , prefix="/ai"     , tags=["Exercises"])
-api_router.include_router(answer_router , prefix="/answers", tags=["Exercises"])
+api_router.include_router(answer_router , prefix="/answer", tags=["Exercises"])
 api_router.include_router(stats_router   , prefix="/stats"  , tags=["Stats"])
