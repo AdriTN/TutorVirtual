d8a6f921ab1ce5e8a16119aa140a91b2e05aaf57
Adrián Talavera <adriiantalavera@gmail.com>
2025-07-01 20:33:04 +0100

    Se ha actualizado el hook useEnrollSubject en el frontend.

--- tutor-frontend/src/features/explore/hooks/useEnrollSubject.ts
+++ tutor-frontend/src/features/explore/hooks/useEnrollSubject.ts
@@ -1,22 +1,36 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { enrollSubject } from "@/services/api";
+import { AxiosError } from "axios";
 
-// Nuevo tipo para los parámetros de mutación
+
 interface EnrollSubjectParams {
   subjectId: number;
   courseId: number;
 }
 
+
 export const useEnrollSubject = () => {
   const qc = useQueryClient();
 
-  return useMutation({
+  return useMutation<
+    unknown,
+    Error,
+    EnrollSubjectParams
+  >({
     mutationKey: ["enrollSubject"],
-    mutationFn : (params: EnrollSubjectParams) =>
+    mutationFn: (params: EnrollSubjectParams) =>
       enrollSubject(params.subjectId, params.courseId),
 
     onSuccess: () => {
       qc.invalidateQueries({ queryKey: ["my-courses"] });
+    },
+    onError: (error: Error) => {
+      if (error instanceof AxiosError && error.response?.status === 409) {
+        const detailMessage = error.response?.data?.detail;
+        if (detailMessage) {
+          error.message = detailMessage;
+        }
+      }
     },
   });
 };
