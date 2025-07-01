debec4ec781b475fd4fe1ab7fc153cd164fb2735
Adrián Talavera <adriiantalavera@gmail.com>
2025-07-01 21:39:09 +0100

    Añadimos los modelos faltantes a las importaciones para asegurar la correcta detección por Alembic autogenerate.

--- tutor-backend/migrations/env.py
+++ tutor-backend/migrations/env.py
@@ -5,6 +5,10 @@ from src.database.base import Base
 from src.models.user import User, UserProvider, RefreshToken
 from src.models.course import Course
 from src.models.subject import Subject
+from src.models.theme import Theme
+from src.models.exercise import Exercise
+from src.models.user_response import UserResponse
+from src.models.user_theme_progress import UserThemeProgress
 
 from logging.config import fileConfig
 
