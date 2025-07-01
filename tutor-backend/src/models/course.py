9a365de59b1e70031774d851ecd9acd656ff90e9
Adrián Talavera <adriiantalavera@gmail.com>
2025-07-01 20:31:16 +0100

    Actualizamos las relaciones en los modelos User, Subject y Course para usar user_enrollments

--- tutor-backend/src/models/course.py
+++ tutor-backend/src/models/course.py
@@ -6,7 +6,7 @@ from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database.base import Base
-from src.models.associations import course_subjects, user_courses
+from src.models.associations import course_subjects, user_courses, user_enrollments 
 
 
 class Course(Base):
@@ -16,13 +16,24 @@ class Course(Base):
     title: Mapped[str]    = mapped_column(String(255), nullable=False, unique=True, index=True)
     description: Mapped[str | None] = mapped_column(Text)
 
+    # Asignaturas que componen este curso
     subjects: Mapped[List["Subject"]] = relationship(
         secondary=course_subjects,
         back_populates="courses",
         lazy="selectin",
     )
+
+    # Estudiantes con acceso general a este curso
     students: Mapped[List["User"]] = relationship(
         secondary=user_courses,
         back_populates="courses",
         lazy="selectin",
+    )
+
+    # Estudiantes matriculados en al menos una asignatura de este curso
+    enrolled_students: Mapped[List["User"]] = relationship(
+        secondary=user_enrollments,
+        back_populates="enrolled_in_courses",
+        viewonly=True,
+        lazy="selectin"
     )
