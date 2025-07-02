04c68ae3a2cacc5d497bf432e1f2b69f675a8052
Adrián Talavera <adriiantalavera@gmail.com>
2025-07-02 09:33:53 +0100

    Se eliminaron los archivos del componente Toast local.

--- tutor-frontend/src/features/admin/components/index.ts
+++ tutor-frontend/src/features/admin/components/index.ts
@@ -2,7 +2,6 @@ export { default as AdminCard   } from "./molecules/AdminCard/AdminCard";
 export { default as CrudModal   } from "./organisms/CrudModal/CrudModal";
 export { default as SelectField } from "./atoms/SelectField/SelectField";
 export { default as TabButton   } from "./atoms/TabButton/TabButton";
-export { default as Toast }       from "./molecules/Toast/Toast";
 export { default as ConfirmDialog } from "./molecules/ConfirmDialog/ConfirmDialog";
 export { default as EntityTable } from "./molecules/EntityTable/EntityTable";
 export { default as MultiCheckList } from "./atoms/MultiCheckList/MultiCheckList";
